bf90cddfe6108251fd7859d3acffb40062bce926
Oscar Orellana <orellana.monterrosa.o@gmail.com>
2023-12-18 15:13:14 -0600

    :bug: fix new trainer function and keep lang

--- src/app/core/services/local-storage.service.ts
+++ src/app/core/services/local-storage.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class LocalStorageService {
   set(key: string, value: any): void {
+    if (typeof value === 'string') {
+      localStorage.setItem(key, value);
+      return;
+    }
     localStorage.setItem(key, JSON.stringify(value));
   }
 
